52478cd69873b052a477db9694f3af8f4502f376
Momo Rao <monicaxrao@gmail.com>
2018-10-16 18:22:00 -0400

    feast: Set up socket.io to synchronize posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import openSocket from 'socket.io-client';
+const socket = openSocket('/'); 
 
 class App extends Component {
   state = {
+    timestamp: 'no timestamp yet',
     response: ''
   };
 
   componentDidMount=()=> {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => res.forEach(this.addMessages))
       .catch(err => console.log(err));
-    this.addMessages({name:'Tim', message:'Hello'})
+    socket.on('message', this.addMessages)
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/messages');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -24,6 +27,20 @@ class App extends Component {
   addMessages = (message)=>{
     document.getElementById('messages').insertAdjacentHTML('afterbegin',`<h4> ${message.name}</h4> <p>${message.message}</p>`)
   }  
+  postMessage = (message)=>{
+    fetch('/messages',{
+      method: 'post',
+      body: JSON.stringify(message),
+      headers: {"Content-Type": "application/json"}
+    })
+  }
+  sendMessage =()=>{
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
+    const msg = {name, message}
+    console.log(msg)
+    this.postMessage(msg)
+  }
 
   render() {
     return (
@@ -31,11 +48,13 @@ class App extends Component {
         <br/>
         <p className="App-intro">{this.state.response}</p>
         <div className="jumbotron">
-          <h1 className="display-4">Send Messageeeeee</h1>
+          <h1 className="display-4">Send Message</h1>
           <br/>
-          <input type="text" className="form-control" placeholder="Name"/>
+          <input id="name" type="text" className="form-control" placeholder="Name"/>
           <br/>
-          <button id="send" className="btn btn-success" onClick={this.componentDidMount}>Send</button>
+          <textarea id="message" className="form-control" placeholder="Message"></textarea>
+          <br/>  
+          <button id="send" className="btn btn-success" onClick={this.sendMessage}>Send</button>
         </div>
         <div id="messages"></div>
       </div>      
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+let bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+let messages = [
+  {name: 'Time', message:'Hi'},
+  {name: 'Jane', message:'Hello'}
+]
+app.get('/messages', (req, res) => {
+  res.send(messages);
 });
+app.post('/messages', (req, res)=>{
+  messages.push(req.body)
+  io.emit('message', req.body)
+  res.sendStatus(200)
+})
+io.on('connection', (socket)=>{
+  console.log('a user connected')
+})
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,4 +32,4 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-app.listen(port, () => console.log(`Listening on port ${port}`));
+http.listen(port, () => console.log(`Listening on port ${port}`));
