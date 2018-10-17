a9c9c0c0a793899582b14290fdc8126d70b91daa
Momo Rao <monicaxrao@gmail.com>
2018-10-17 11:48:56 -0400

    feast: Add mongoDB, mlab and manage with Mongoose

--- server.js
+++ server.js
@@ -5,22 +5,33 @@ const app = express();
 let http = require('http').Server(app)
 let io = require('socket.io')(http)
 const port = process.env.PORT || 5000;
+let mongoose = require('mongoose')
+const dbUrl = 'mongodb://user:user12345678@ds235243.mlab.com:35243/learning-node'
+let Message = mongoose.model('Message',{
+  name: String,
+  message: String
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
-// API calls
-let messages = [
-  {name: 'Time', message:'Hi'},
-  {name: 'Jane', message:'Hello'}
-]
-app.get('/messages', (req, res) => {
-  res.send(messages);
-});
-app.post('/messages', (req, res)=>{
-  messages.push(req.body)
-  io.emit('message', req.body)
-  res.sendStatus(200)
+
+app.get('/messages', (req, res)=>{
+  Message.find({},(err, messages)=>{
+      res.send(messages)
+  })
 })
+
+app.post('/messages', (req, res)=>{
+  let message = new Message(req.body)
+  message.save((err)=>{
+      if(err){sendStatus(500)}
+      else {
+          io.emit('message', req.body)
+          res.sendStatus(200)
+      }
+  })
+})
+
 io.on('connection', (socket)=>{
   console.log('a user connected')
 })
@@ -32,4 +43,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
+mongoose.connect(dbUrl,{useNewUrlParser: true},(err)=>{
+  console.log('mongo db connection', err)
+})
+
 http.listen(port, () => console.log(`Listening on port ${port}`));
